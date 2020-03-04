d3f720861fc8746167611d61f32169cfc8cc8aab
sai.shi <sai.shi@vipshop.com>
2020-03-04 18:08:06 +0800

    提交

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:xiaoSais/xiaoSais.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
